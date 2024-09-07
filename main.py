219a62ff9f0ac7b1e8825ac27f08542d0e08d977
nicosup <damiconicola98@gmail.com>
2024-09-07 18:19:51 -0400

    refactorizado con numba y numpy

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
-from random import randint, seed
-from time import time_ns
-from operator import itemgetter
+import numpy as np
+from numba import njit
 
+@njit
 def ebrio():
-    seed(time_ns())
-    cordenadas = {"x": 0, "y": 0}
-    camino_ebrio = {
-        1: (-1, 0),
-        2: (1, 0),
-        3: (0, 1),
-        4: (0, -1)
-    }
-    for _ in range(9):
-        direccion = randint(1, 4)
-        camino_x, camino_y = camino_ebrio[direccion]
-        cordenadas["x"] += camino_x
-        cordenadas["y"] += camino_y
+    # Usamos un array en lugar de un diccionario para almacenar las coordenadas
+    cordenadas = np.array([0, 0])
+    camino_ebrio = np.array([
+        [-1, 0],  # dirección 1
+        [1, 0],   # dirección 2
+        [0, 1],   # dirección 3
+        [0, -1]   # dirección 4
+    ])
+    
+    for _ in range(10):
+        direccion = np.random.randint(0, 4)  
+        cordenadas += camino_ebrio[direccion]
+    
     return cordenadas
 
-
+@njit
 def simulacion():
-    n_simulacion = 100 * 100
+    n_simulacion = 1000* 500
     n_is_far = 0
-    data = []
-    for _ in range(1,n_simulacion):
-        x,y = itemgetter("x","y")(ebrio())
-        data.append((x,y))
-        if x >= 2 or y >= 2:
-            n_is_far +=1
+    data = np.zeros((n_simulacion, 2))
+    
+    for i in range(n_simulacion):
+        x, y = ebrio()
+        data[i, 0] = x
+        data[i, 1] = y
+        if abs(x) >= 2 or abs(y) >= 2:
+            n_is_far += 1
             
-    return {"probabilidad": n_is_far / n_simulacion * 100, "data":data}
-
+    return n_is_far / n_simulacion * 100, data
 
 if __name__ == "__main__":
-    print(f"probabilidad de que el ebrio este a 2 calles de su inicio: {simulacion()["probabilidad"]}") # ilegalisimo
+    probabilidad, _ = simulacion()
+    print(f"Probabilidad de que el ebrio esté a 2 calles de su inicio: {probabilidad}%") 
