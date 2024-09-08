b7b8b3d2448d44ab9e2f0acefba153710f9adba3
nicosup <damiconicola98@gmail.com>
2024-09-08 17:40:06 -0400

    some logical fix + more output info

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import numpy as np
 from numba import njit
+import time
+
 
 @njit
 def ebrio():
     # Usamos un array en lugar de un diccionario para almacenar las coordenadas
+    
     cordenadas = np.array([0, 0])
     camino_ebrio = np.array([
-        [-1, 0],  # dirección 1
-        [1, 0],   # dirección 2
-        [0, 1],   # dirección 3
-        [0, -1]   # dirección 4
+        [-1, 0],  # oeste
+        [1, 0],   # este
+        [0, 1],   # norte
+        [0, -1]   # sur
     ])
+    direcciones = np.floor(np.random.uniform(0, 4, 10)).astype(np.int16)
     
-    for _ in range(10):
-        direccion = np.random.randint(0, 4)  
+    for direccion in direcciones:
         cordenadas += camino_ebrio[direccion]
     
     return cordenadas
 
 @njit
-def simulacion():
-    n_simulacion = 1000* 500
+def simulacion(n_simulacion = 1000 * 500):
     n_is_far = 0
     data = np.zeros((n_simulacion, 2))
     
@@ -28,11 +30,17 @@ def simulacion():
         x, y = ebrio()
         data[i, 0] = x
         data[i, 1] = y
-        if abs(x) >= 2 or abs(y) >= 2:
+        if abs(x) + abs(y) == 2:
             n_is_far += 1
             
-    return n_is_far / n_simulacion * 100, data
+    return n_is_far / n_simulacion * 100, data,n_is_far,n_simulacion
+        
+        
+    
 
 if __name__ == "__main__":
-    probabilidad, _ = simulacion()
+    probabilidad, data,n_is_far,n_simulacion = simulacion(100000)
     print(f"Probabilidad de que el ebrio esté a 2 calles de su inicio: {probabilidad}%") 
+    print("mas info".center(20,"-"))
+    print(f"numero de simulaciones: {n_simulacion}")
+    print(f"numero de veces que el ebrio esta a 2 calles: {n_is_far}")
--- old_main.py
+++ old_main.py
@@ -5,32 +5,35 @@ from operator import itemgetter
 def ebrio():
     seed(time_ns())
     cordenadas = {"x": 0, "y": 0}
-    camino_ebrio = {
-        1: (-1, 0),
-        2: (1, 0),
-        3: (0, 1),
-        4: (0, -1)
-    }
-    for _ in range(9):
-        direccion = randint(1, 4)
+    camino_ebrio = [
+        (-1, 0),
+        (1, 0),
+        (0, 1),
+        (0, -1)
+    ]
+    for _ in range(10):
+        direccion = randint(0,3)
         camino_x, camino_y = camino_ebrio[direccion]
         cordenadas["x"] += camino_x
         cordenadas["y"] += camino_y
     return cordenadas
 
 
-def simulacion():
-    n_simulacion = 1000 * 1000
+def simulacion(n_simulacion = 1000 * 500):
     n_is_far = 0
     data = []
     for _ in range(1,n_simulacion):
         x,y = itemgetter("x","y")(ebrio())
         data.append((x,y))
-        if abs(x) >= 2 or abs(y) >= 2:
+        if abs(x) + abs(y) == 2:
             n_is_far +=1
             
-    return {"probabilidad": n_is_far / n_simulacion * 100, "data":data}
+    return n_is_far / n_simulacion * 100, data, n_is_far, n_simulacion
 
 
 if __name__ == "__main__":
-    print(f"probabilidad de que el ebrio este a 2 calles de su inicio: {simulacion()["probabilidad"]}") # ilegalisimo
+    probabilidad,data,n_is_far,n_simulacion = simulacion()
+    print(f"probabilidad de que el ebrio este a 2 calles de su inicio: {probabilidad}")
+    print("mas info".center(20,"-"))
+    print(f"numero de simulaciones: {n_simulacion}")
+    print(f"numero de veces que el ebrio esta a 2: {n_is_far}")
